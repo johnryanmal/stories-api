9ff150d21e888c93d74f03369891c19b8dcbdc16
johnryanmal <johnryanmal@gmail.com>
2023-02-07 13:58:55 -0800

    chore: seed dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create!([
+  {name: "Test name", email: "test@test.com", password_digest: "$2a$12$rpiSVj4Zrmz/eo347yjaXurIxqfH.iYjlsdoPVBd7saReYgJ2VDEm"}
+])
+Story.create!([
+  {title: "My story", graph: {"start"=>"It all begins here..."}, user_id: 1, public: true},
+  {title: "My story", graph: {"nodes"=>[{"id"=>"1", "title"=>"???", "type"=>"text", "x"=>-200, "y"=>200}, {"id"=>"2", "title"=>"Advice", "type"=>"button", "x"=>0, "y"=>300}, {"id"=>"3", "title"=>"Help", "type"=>"button", "x"=>0, "y"=>100}, {"id"=>"4", "title"=>"To chat!", "type"=>"chat", "x"=>200, "y"=>200}], "edges"=>[{"source"=>"1", "target"=>"2", "type"=>"empty"}, {"source"=>"2", "target"=>"4", "type"=>"empty"}, {"source"=>"1", "target"=>"3", "type"=>"empty"}, {"source"=>"3", "target"=>"4", "type"=>"empty"}]}, user_id: 1, public: false}
+])
